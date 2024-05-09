947f8b1b02a847501dac4c260cbf2274aadbf5a8
Massatrio16 <dimassetosatrio@gmail.com>
2024-05-09 11:13:50 +0700

    First

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_X663.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_X663-user \
+    twrp_X663-userdebug \
+    twrp_X663-eng
